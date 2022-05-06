ab4a22d715fa1486a1ec277729306ae2d57e533d
chiragaherwar013 <60267760+chiragaherwar013@users.noreply.github.com>
2022-05-07 00:36:58 +0530

    Cleaned COde | Updated Libs

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { CurrentUserProvider } from "./Context/userContext";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { SnackbarProvider } from "notistack";
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
   <React.StrictMode>
     <CurrentUserProvider>
       <SnackbarProvider maxSnack={3}>
+        <HelmetProvider >
         <App />
+        </HelmetProvider>
       </SnackbarProvider>
     </CurrentUserProvider>
   </React.StrictMode>,
